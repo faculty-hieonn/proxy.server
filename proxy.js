e7c6311829258fcf178967588ee86a5f9a547e1c
awesomeus <hello.awesomeus@gmail.com>
2024-10-14 16:44:52 +0900

    proxy

---
+++ proxy.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/api', createProxyMiddleware({ 
+  target: 'https://script.google.com/macros/s/AKfycbymucHyMeANwDRi7xtl0IbXppo4PJt8DgWmsAK4g-KMBKuZ6veqCZymTy2GpVqPVLX5/exec',
+  changeOrigin: true,
+}));
